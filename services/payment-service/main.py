4b6a18ef4a40bae8a3a3052b13ce67420a5cdde5
Mario Hernandez <maiker1422@outlook.com>
2026-04-02 11:30:54 -0600

    Revert "se agrega webhook"
    This reverts commit b8e48e3741134a537925685feb748212c8de5f13.

--- services/payment-service/main.py
+++ services/payment-service/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
 from app.controllers.payment_controller import router as payment_router
-from app.controllers.webhook_controller import router as webhook_router
 from app.db.database import engine, Base
 
 app = FastAPI()
 
 app.include_router(payment_router, prefix="/payments")
-app.include_router(webhook_router)
 
 Base.metadata.create_all(bind=engine)
